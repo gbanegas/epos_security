40cdc957988f904f14d5798c370070a69582ef42
Gustavo Souza Banegas <g.banegas@inf.ufsc.br>
2011-06-01 10:34:21 -0300

    removido arquivos que nao seram mais usados.

--- include/security/evp.h
+++
@@ -1,8 +0,0 @@
-
-
-#define EVP_MAX_MD_SIZE			64	/* longest known is SHA512 */
-#define EVP_MAX_KEY_LENGTH		32
-#define EVP_MAX_IV_LENGTH		16
-#define EVP_MAX_BLOCK_LENGTH		32
-
-#define EVP_PKEY_NONE	NID_undef
